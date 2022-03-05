192ce52fd9c8d9d67a9462ecf672bc2fcc957729
Rami Sabbagh <rami.sab07@gmail.com>
2022-03-05 14:50:33 +0200

    Force dark mode

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,6 +42,10 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: true,
+      },
       tableOfContents: {
         minHeadingLevel: 2,
         maxHeadingLevel: 4,
